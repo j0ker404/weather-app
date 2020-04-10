e522c3fcbc1efa23fe17311a6e6a018f96c5d742
joker <58110797+j0ker404@users.noreply.github.com>
2020-04-10 22:27:34 +0200

    basic features implementd

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # weather-app
 Simple weather app
+
+This weather app uses OpenWeatherMap api
+for weather data. Axios is used to make api requests.
+
+The app takes in a city name as input and then
+displays the temperature and weather description.
--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-let searchbar = document.getElementById("searchbar");
-
-// add input event
-/**
- * when input is updated then
- */
-searchbar.addEventListener("change", function () {
-    alert("test");
-});
-
-cityList = [];
+let cityList = [];
 // parallel arrays
 let cityNames = [];
 let cityIds = [];
@@ -28,7 +18,7 @@ let idFilter = function (array) {
 axios.get('/city.list.json')
     .then(function (response) {
         // handle success
-        console.log('success');
+        // console.log('success');
         // console.log(response.data);
 
         // fill up arrays
@@ -42,5 +32,77 @@ axios.get('/city.list.json')
     });
 
 
+// main code
+/**
+ * openweathermap api must be used here
+ */
+const apiKey = "add-api-key-here";
 
+
+let updateResults = false;
+let searchbar = document.getElementById("searchbar");
+
+/**
+ * Function calls
+ */
+let update = function () {
+    // console.log(searchbar.value);
+    let id;
+    let val = searchbar.value;
+    val = (val.trim().toLocaleLowerCase()).split(" ").join(" ");
+    // searchbar.value = "";
+    let match = function (value) {
+        return value === val;
+    }
+    index = cityNames.findIndex(match);
+    // console.log(index);
+    // if index exists
+    if (index !== -1) {
+        id = cityIds[index];
+        // units=metric
+        // perfom get request
+        const base = "https://api.openweathermap.org/data/2.5/weather?";
+        let unit = "units=metric";
+        let idStr = "id=" + id;
+        let apiStr = "appid="+apiKey;
+        let url = base + idStr + "&" + apiStr + "&" + unit; 
+        axios.get(url)
+            .then(function (response) {
+                // handle success
+                // console.log('success: got weather');
+                // console.log(response.data);
+
+                // update weather description and temperature to 'currendData'
+                // let currentData = [];
+                // console.log(response.data.main["temp"]);
+                // currentData[0] = (response.data.main["temp"]);
+                // currentData[1] = (response.data.weather[0]["description"]);
+                // console.log(currentData);
+                
+                // update results on front end 
+                document.getElementById("temp-unit").style.display = "inline";
+                document.getElementById("temp-val").innerHTML = Math.round(response.data.main["temp"]);
+                document.getElementById("type-val").innerHTML = response.data.weather[0]["description"];
+
+            })
+            .catch(function (error) {
+                console.log("Error with weather request");
+                console.log(error);
+            });
+    } else {
+        // invalid data inputed
+        document.getElementById("temp-unit").style.display = "none";
+        document.getElementById("temp-val").innerHTML = "Enter valid";
+        document.getElementById("type-val").innerHTML = "city name";
+    }
+}
+// add input event
+/**
+ * when input is updated then
+ */
+searchbar.addEventListener("change", function () {
+
+    update();
+
+});
 
